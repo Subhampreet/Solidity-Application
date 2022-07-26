3299cded130031658916e879695ee47e3975d85f
Subhampreet <subhampreet18@gmail.com>
2022-07-26 22:32:13 +0530

    Faucet Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='faucet-wrapper'>
+        <div className='faucet'>
+            <div className='balance-view is-size-2'>
+              Current Balance: <strong>10</strong> ETH
+            </div>
+            <div className='btn mr-2'>Donate</div>
+            <div className='btn'>Withdraw</div>
+        </div>
+      </div>
+    </>
   );
 }
 
