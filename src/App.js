1d307370e5ba941917dc1b89dbc3cff9e7bd91d2
Subhampreet <subhampreet18@gmail.com>
2022-07-29 15:45:42 +0530

    Get Accounts Method Implemented

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
     web3: null
   })
 
+  const [account, setAccount] = useState(null)
+
   useEffect(() => {
     const loadProvider = async () => {
       let provider = null;
       if(window.ethereum){
         provider = window.ethereum;
         try{
-          await provider.enable();
+          await provider.request({method : "eth_requestAccounts"});
         }
         catch {
           console.error("User denied accounts access!");
@@ -36,13 +38,27 @@ function App() {
     loadProvider()
   }, [])
 
-  console.log(web3Api.web3)
+  useEffect(() => {
+    const getAccounts = async() => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+
+    web3Api.web3 && getAccounts()
+  }, [web3Api.web3])
+
 
 
   return (
     <>
       <div className='faucet-wrapper'>
         <div className='faucet'>
+            <span>
+              <strong>Account:</strong>
+            </span>
+            <h1>
+              {account ? account: "not connected"}
+            </h1>
             <div className='balance-view is-size-2'>
               Current Balance: <strong>10</strong> ETH
             </div>
